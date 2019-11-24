cdc5327d15fae887d934a9fc99f47a3aa064885f
YiannisKalt <yiannis.kaltampanidis@gmail.com>
2019-11-24 21:09:41 +0200

    new file:   cnn/saved_model.pb 	new file:   tools_no_librosa.py

---
+++ tools_no_librosa.py
@@ -0,0 +1,56 @@
+import os
+import soundfile as sf
+import numpy as np
+from scipy import signal
+from tensorflow import keras
+from tensorflow.keras.utils import to_categorical
+
+def aesdd_spectrogram():
+    
+    # (1) Load and subsample audio files 44kHz - 11kHz#
+    speech_signals = []
+    sentiments_dir = {'anger': 0, 'disgust': 1, 'fear': 2, 'happiness':3, 'sadness': 4}
+    dataset_path = './Acted Emotional Speech Dynamic Database/'
+
+    for sentiment in os.listdir(dataset_path):
+        for audiofile in os.listdir(dataset_path + sentiment):
+            audio_path = dataset_path + sentiment + '/' + audiofile
+            try:
+                speech_signal, sr = sf.read(audio_path)
+                speech_signal, sr = signal.decimate(speech_signal, 4), sr // 4
+                speech_signals.append((speech_signal, sentiments_dir[sentiment]))
+            except:
+                pass #In case of broken audiofiles
+     
+    # Zero_padd signals to match maximum duration
+
+    max_dur = max([len(k[0]) for k in speech_signals])
+
+    samples = np.array([np.append(k[0], np.zeros(max_dur - len(k[0]))) 
+                       for k in speech_signals]) 
+
+    labels = np.array([k[1] for k in speech_signals]) 
+
+    # Compute Spectrogram For Each Utterance 
+    spectro = np.array([signal.spectrogram(k, nperseg = sr // 4, noverlap = sr // 8)[-1] 
+                        for k in samples])
+    spectro = spectro.reshape((spectro.shape[0], spectro.shape[1], spectro.shape[2], 1))
+    return spectro, to_categorical(labels)
+
+
+def split_training_set(data, labels, train_percentage= 2 / 3):
+    random_indices = np.random.permutation(range(len(data)))
+    n_training = np.int(np.ceil(train_percentage * data.shape[0]))
+    train_indices = random_indices[:n_training]
+    test_indices = random_indices[n_training:]
+
+    X_train, Y_train = data[train_indices], labels[train_indices]
+    X_test, Y_test = data[test_indices], labels[test_indices]
+    return X_train, X_test, Y_train, Y_test
+
+    
+
+    
+
+    
+
